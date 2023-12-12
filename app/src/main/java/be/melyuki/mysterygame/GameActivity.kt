9490788e0a2a8cf020a294623c6b0cd88e50e521
Melvin Taïder <tmneo1989@hotmail.com>
2023-12-12 14:53:55 +0100

    *.kt_Commented

--- app/src/main/java/be/melyuki/mysterygame/GameActivity.kt
+++ app/src/main/java/be/melyuki/mysterygame/GameActivity.kt
@@ -15,14 +15,18 @@ import com.google.android.material.internal.ViewUtils.hideKeyboard
 
 class GameActivity : AppCompatActivity() {
 
+    // Création d'un objet compagnion,
+    // qui servira à transporter les données d'une activité à une autre.
     companion object {
         const val USERNAME = "USERNAME"
         const val PREV_GAME = "PREV_GAME"
         const val PREV_SCORE = "PREV_SCORE"
     }
 
+    // Binding des éléments xml du layout et de l'activity.
     lateinit var binding : ActivityGameBinding
 
+    // Création des variables globales.
     private lateinit var username : String
     private var cpu : Int = 0
     private var attempt : Int = 3
@@ -32,87 +36,121 @@ class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Adaptation de l'incorporation du layout suite au binding ci-dessus.
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Fonction qui choisi un numéro aléatoire.
         cpuRandom()
 
+        // Récupération du nom du joueur via l'intent, venant de l'autre activité.
         username = intent.getStringExtra(USERNAME).toString()
 
+        // Affichage du nom du joueur dans l'élément xml dédié.
         binding.tvGameUsername.text = getString(R.string.welcome_username, username)
+        // On cache la réponse, non désirée pour l'instant, d'après choix du joueur.
         binding.tvGameWrong.visibility = View.GONE
 
+        // Création du callback (listener) et de l'action à produire suite au click du bouton.
         binding.btnGameSend.setOnClickListener { cpuVsUser() }
 
     }
 
+    // le @SuppressLint(...),
+    // permet de cacher l'erreur produite à cause de l'utilisation de la fonction hideKeyboard().
     @SuppressLint("RestrictedApi")
     private fun cpuVsUser() {
-        val user : Int = binding.etGameUser.text.toString().toInt()
-//        Log.d("TAG", "cpuVsUser: $user")
 
+        // Création d'une variable qui va contenir le numéro du joueur.
+        val user : Int = binding.etGameUser.text.toString().toInt()
+        // Log.d("TAG", "cpuVsUser: $user")
+
+        // Application de plusieurs choses sur l'EditText du choix du joueur.
         binding.etGameUser.apply {
+            // On nettoie l'EditText
             text.clear()
+            // On enlève le focus sur l'EditText
             clearFocus()
-            val view : View = this.rootView
-            hideKeyboard(view)
+
+            // On cache le clavier du mobile.
+            hideKeyboard(this.rootView)
         }
 
+        // Conditions et Comparaisons du choix du joueur et du numéro random.
         if (user < 0 || user > 10) {
+            // Gestion d'erreur d'utilisation du joueur.
             Toast.makeText(this, getString(R.string.toast_game_error), Toast.LENGTH_LONG).show()
         }
         else {
             if (user < cpu) {
-                binding.tvGameWrong.text = getString(R.string.game_number_is_higher)
-                refreshAttempts()
+                // Création d'une variable contenant l'indice à afficher.
+                val msgHigher : String = getString(R.string.game_number_is_higher)
+                // Fonction qui rafraichit le nombre d'essais du joueur.
+                refreshAttempts(msgHigher)
             }
             else if (user > cpu) {
-                binding.tvGameWrong.text = getString(R.string.game_number_is_lower)
-                refreshAttempts()
+                // Création d'une variable contenant l'indice à afficher.
+                val msgLower : String = getString(R.string.game_number_is_lower)
+                // Fonction qui rafraichit le nombre d'essais du joueur.
+                refreshAttempts(msgLower)
             }
+            // Fonction appelée une fois le bon numéro trouvé.
             else backToTheFuture()
         }
     }
 
-    private fun refreshAttempts() {
+    private fun refreshAttempts(message : String) {
+        // Message envoyé dans la fenêtre d'indice si le numéro du joueur est plus petit.
+        binding.tvGameWrong.text = message
+        // Affichage de la fenêtre d'indice au joueur.
         binding.tvGameWrong.visibility = View.VISIBLE
+
+        // On enlève un essai au joueur.
         attempt--
 
+        // On rafraichit les essais
         binding.tvGameAttempts.text =
             if (attempt > 1) getString(R.string.tv_game_show_attempts, attempt)
             else getString(R.string.tv_game_show_attempt, attempt)
 
+        // Fonction appelée une fois que le joueur n'a plus d'essai.
         if(attempt == 0) backToTheFuture()
     }
 
     private fun backToTheFuture() {
 
+        // Fonction qui rajoute les scores précédents.
         addPrevScore()
 
-//        Log.d("TAG", "backToTheFuture: game = $game / score = $attempt")
+        // Log.d("TAG", "backToTheFuture: game = $game / score = $attempt")
 
+        // Création de l'intent qui nous ramènera à la MainActivity, avec les données nécessaires.
         val intent : Intent = Intent(this, MainActivity::class.java).apply {
             putExtra(GAME, game)
             putExtra(SCORE, score)
             putExtra(PREV_USER, username)
         }
+        // Lancement de l'intent vers la MainActivity.
         startActivity(intent)
     }
 
     private fun addPrevScore() {
 
+        // Récupération du nombre de jeu déjà joués.
         val prevGame : Int = intent.getIntExtra(PREV_GAME, 0)
+        // Addition de ce jeu-ci et des précédents.
         game = prevGame + 1
-        // game ++
 
+        // Récupération des scores obtenus précédemment.
         val prevScore : Int = intent.getIntExtra(PREV_SCORE, 0)
+        // Addition des score de cette partie-ci et des précédents.
         score = prevScore + attempt
     }
 
     private fun cpuRandom() {
         // Méthode JAVA
-//        val rand = Math.random().toInt()
-//        cpu = (rand * 10)
+        // val rand = Math.random().toInt()
+        // cpu = (rand * 10)
 
         // Méthode pure Kotlin
         val rand = (0..10).shuffled().first()
@@ -123,8 +161,9 @@ class GameActivity : AppCompatActivity() {
 
     @SuppressLint("MissingSuperCall")
     override fun onBackPressed() {
-//        super.onBackPressed()
+        // En commentant la ligne ci-dessous, on interdit l'utilisation du bouton back du mobile!
+        // super.onBackPressed()
 
-//        Log.d("TAG", "onBackPressed: disable")
+        // Log.d("TAG", "onBackPressed: disable")
     }
 }
--- app/src/main/java/be/melyuki/mysterygame/MainActivity.kt
+++ app/src/main/java/be/melyuki/mysterygame/MainActivity.kt
@@ -13,27 +13,36 @@ import be.melyuki.mysterygame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    // Création d'un objet compagnion,
+    // qui servira à transporter les données d'une activité à une autre.
     companion object {
         const val GAME = "GAME"
         const val SCORE = "SCORE"
         const val PREV_USER = "PREV_USER"
     }
 
+    // Binding des éléments xml du layout et de l'activity.
+    // PREALABLEMENT l'activer dans le build.gradle(Module:app)!
     lateinit var binding : ActivityMainBinding
 
+    // Création des variables globales.
     private var game : Int = 0
     private var score : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Adaptation de l'incorporation du layout suite au binding ci-dessus.
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Fonction qui cache des éléments au départ.
         hideElements()
+        // Fonction qui rafraichit les scores.
         refreshScore()
 
         // region LISTENER
+        // Création des callback (listener) et des actions à produire suite au click des boutons.
         binding.btnPlay.setOnClickListener { goGame() }
         binding.btnReset.setOnClickListener { resetGame() }
         binding.btnReplay.setOnClickListener { playAgain() }
@@ -42,71 +51,93 @@ class MainActivity : AppCompatActivity() {
 
     private fun playAgain() {
 
-        val prevUser : String = intent.getStringExtra(PREV_USER).toString()
+        // Création d'une variable qui va contenir le nom de l'utilisateur précedent.
+        val prevUser : String = intent.getStringExtra(PREV_USER)!!
 
+        // Création de l'intent de navigation, munit des données nécessaires.
         val intent : Intent = Intent(this, GameActivity::class.java).apply {
             putExtra(USERNAME, prevUser)
             putExtra(PREV_GAME, game)
             putExtra(PREV_SCORE, score)
         }
+        // Lancement de l'intent vers l'autre activité.
         startActivity(intent)
-    }
-
-    private fun refreshScore() {
-
-        game = intent.getIntExtra(GAME, 0)
-        score = intent.getIntExtra(SCORE, 0)
-//        Log.d("TAG", "game: $game / score: $score")
-
-        if (game > 0) {
-            binding.tvDefaultScore.text = getString(R.string.tv_main_game_score, game, score)
-            binding.tvDefaultResult.text =
-                if(score > 0) getString(R.string.tv_main_result_win)
-                else getString(R.string.tv_main_result_loose)
-
-            showElements()
-        }
-    }
-
-    private fun goGame() {
-
-        val username : String = binding.etMainUsername.text.toString()
-//        Log.d("TAG", "goGame: $username")
-
-        val intent : Intent = Intent(this, GameActivity::class.java).apply {
-            putExtra(USERNAME, username)
-        }
-
-        if (username == ""){
-//            Log.d("TAG", "goGame: test if")
-            Toast.makeText(this, getString(R.string.toast_main_play), Toast.LENGTH_LONG).show()
-        }
-        else
-            startActivity(intent)
     }
 
     private fun resetGame() {
 
+        // Remise à zéro des scores.
         binding.tvDefaultScore.text = getString(R.string.tv_main_game_score, game, score)
+
+        // On cache à nouveau les éléments de départ.
         hideElements()
+    }
+
+    private fun goGame() {
+
+        // Création d'une variable qui va contenir la valeur reçue de l'EditText de l'xml.
+        val username : String = binding.etMainUsername.text.toString()
+        // Log.d("TAG", "goGame: $username")
+
+        // Création de l'intent de navigation vers une autre activité.
+        val intent : Intent = Intent(this, GameActivity::class.java).apply {
+            // putExtra() permet d'envoyer le nom du joueur vers l'autre actitivé.
+            putExtra(USERNAME, username)
+        }
+
+        // Condition d'accès au jeu!
+        if (username == ""){
+            // Log.d("TAG", "goGame: test if")
+
+            // Création d'un toast alertant l'utilisateur.
+            Toast.makeText(this, getString(R.string.toast_main_play), Toast.LENGTH_LONG).show()
+        }
+        else
+            // Lancement de l'intent vers l'autre activité.
+            startActivity(intent)
+    }
+
+    private fun refreshScore() {
+
+        // Récupération des scores de l'intent, venant de l'autre activité.
+        game = intent.getIntExtra(GAME, 0)
+        score = intent.getIntExtra(SCORE, 0)
+        // Log.d("TAG", "game: $game / score: $score")
+
+        // Condition de rafraichissement des scores.
+        if (game > 0) {
+            // Affichage des nouveaux scores dans l'élément xml dédié.
+            binding.tvDefaultScore.text = getString(R.string.tv_main_game_score, game, score)
+            // Affichage du résultat (gagné ou perdu) du jeu dernièrement joué.
+            binding.tvDefaultResult.text =
+                if(score > 0) getString(R.string.tv_main_result_win)
+                else getString(R.string.tv_main_result_loose)
+
+            // Fonction qui affiche les éléments cachés au départ.
+            showElements()
+        }
     }
 
     private fun hideElements() {
 
+        // On cache des éléments
         binding.tvDefaultResult.visibility = View.GONE
         binding.btnReplay.visibility = View.GONE
         binding.btnReset.visibility = View.GONE
 
+        // On affiche des éléments
         binding.etMainUsername.visibility = View.VISIBLE
         binding.btnPlay.visibility = View.VISIBLE
     }
 
     private fun showElements() {
 
+        // On affiche des éléments
         binding.tvDefaultResult.visibility = View.VISIBLE
         binding.btnReplay.visibility = View.VISIBLE
         binding.btnReset.visibility = View.VISIBLE
 
+        // On cache des éléments
         binding.etMainUsername.visibility = View.GONE
         binding.btnPlay.visibility = View.GONE
     }
